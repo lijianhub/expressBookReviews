67d321155b146395648c5544bd41c75ac25b9f38
jianli <405180462@qq.com>
2025-05-24 23:18:11 +1200

    Implement task 2: Get book details based on ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,14 +12,15 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.send(JSON.stringify(books,null, 4));
+  return res.send(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  let book = Object.values(books).find(book =>
+    book.isbn === isbn);
+  return res.send(book);
 });
 
 // Get book details based on author
